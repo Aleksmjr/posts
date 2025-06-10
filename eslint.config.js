e09fff5ea2cd2bd0d7e16869dea64cbe9d34dd2f
«Aleksei <Aleksmjr97@gmail.com>
2025-06-10 12:36:52 +0300

    Fix[esLint]: change esLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,25 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-undef': 'error',
+      'no-unused-vars': 'error',
+      'no-empty': 'error',
+      eqeqeq: 'error',
+      curly: 'error',
+    },
+  },
 ]);
