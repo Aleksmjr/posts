9dbfb600f66f0889460f9efe2a29d89404e0aff1
«Aleksei <Aleksmjr97@gmail.com>
2025-06-27 18:35:39 +0300

    Update: create 5 buttons in footer (next in ui html)

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,4 +1,6 @@
 import { createElement } from '../../helpers/createElement';
+import { Button } from '../../ui/button/button';
+import './footer.css';
 
 export function createFooter() {
   // создаем футер
@@ -13,8 +15,62 @@ export function createFooter() {
     className: 'footer__text',
   });
   // добавляем эти элементы внутрь футера
-  footer.appendChild(container);
-  container.appendChild(text);
 
+  const btn = new Button({
+    text: 'Загрузить еще',
+    attrs: { type: 'button' },
+    className: 'footer__button',
+    icon: 'icon-check',
+    onClick: () => {
+      console.log('Я - стандартная кнопка');
+    },
+  });
+  const btn2 = new Button({
+    text: 'Продолжить',
+    attrs: { type: 'button' },
+    className: 'footer__button',
+    mod: 'secondary',
+    icon: 'icon-check',
+    onClick: () => {
+      console.log('Я - кнопка secondary');
+    },
+  });
+  const btn3 = new Button({
+    text: 'Опасная кнопка',
+    attrs: { type: 'button' },
+    className: 'footer__button',
+    mod: 'danger',
+    icon: 'icon-check',
+    onClick: () => {
+      console.log('Я - кнопка danger');
+    },
+  });
+  const btn4 = new Button({
+    text: 'Длинющая кнопенция',
+    attrs: { type: 'button' },
+    className: 'footer__button',
+    icon: 'icon-check',
+    wide: 'true',
+    onClick: () => {
+      console.log('А я - кнопка на всю ширину');
+    },
+  });
+  const btn5 = new Button({
+    text: 'А меня ты не нажмешь',
+    attrs: { type: 'button', disabled: '' },
+    className: 'footer__button',
+    icon: 'icon-check',
+    onClick: () => {
+      console.log('click - тут потом будет событие');
+    },
+  });
+  container.appendChild(text);
+  container.appendChild(btn.button);
+  container.appendChild(btn2.button);
+  container.appendChild(btn3.button);
+  container.appendChild(btn4.button);
+  container.appendChild(btn5.button);
+
+  footer.appendChild(container);
   return footer;
 }
