485256b5e8d0e53808e951a4c7763a1c597b7e0f
«Aleksei <Aleksmjr97@gmail.com>
2025-08-12 18:06:35 +0300

    Update: created and feat header component and header.module.scss with styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,30 +1,50 @@
 import { createElement } from '../../helpers/createElement';
+import styles from './header.module.scss';
+import { Button } from '../../ui/button/button';
+import clsx from 'clsx';
 
 export function createHeader() {
-  const header = createElement({ tag: 'header', className: 'header' });
-  const container = createElement({ tag: 'div', className: 'container' });
-
-  const navItems = [
-    { text: 'Главная', href: '/' },
-    { text: 'О нас', href: '/about' },
-    { text: 'Посты', href: '/posts' },
-    { text: 'Блог', href: '/blog' },
-  ];
-
-  const nav = createElement({ tag: 'nav', className: 'header__nav' });
-
-  navItems.forEach((item) => {
-    nav.appendChild(
-      createElement({
-        tag: 'a',
-        className: 'header__link',
-        content: item.text,
-        href: item.href,
-      }),
-    );
+  const header = createElement({ tag: 'header', className: styles.header });
+  const container = createElement({
+    tag: 'div',
+    className: clsx(styles.header__container, 'container'),
   });
 
+  const logo = createElement({
+    tag: 'a',
+    className: styles.header__logo,
+    href: '/',
+  });
+
+  const nav = createElement({ tag: 'nav', className: styles.header__nav });
+
+  [
+    { href: '/', text: 'About' },
+    { href: '/contacts', text: 'Contacts' },
+    { href: '/posts', text: 'Posts' },
+  ]
+    .map(
+      (item) =>
+        new Button({
+          ...item,
+          classNames: styles.header__link,
+          mod: 'header',
+        }),
+    )
+    .forEach((el) => nav.appendChild(el));
+
+  const signUser = new Button({
+    text: 'Sign',
+    className: 'header__sign',
+    mod: 'header',
+    href: '#',
+    onClick: () => {
+      console.log('я буду переходить в личный кабинет');
+    },
+  });
+  container.appendChild(logo);
   container.appendChild(nav);
+  container.appendChild(signUser);
   header.appendChild(container);
 
   return header;
