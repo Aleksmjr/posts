ee1d6d5b6dd89334afbfcffb9234666156ccc0c3
«Aleksei <Aleksmjr97@gmail.com>
2025-08-26 13:16:29 +0300

    Feat: created posts page and styles

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,13 +1,25 @@
 // создает содержимое внутри main, не имея отношения к header и footer
 // класс, который наследуется от pages (генерит новую разметку). При этом роутинг должен найти див рут и запихать туда этот эбаут. Определить роутинг можно через массив роутов, где каждый роут - это объект с полями path и component.
 import { createElement } from '../helpers/createElement';
+import { appendFabric } from '../helpers/appendFabric';
+import clsx from 'clsx';
+import styles from '../components/posts/posts.module.scss';
 
-export function createPostsPage(root) {
-  const section = createElement({ tag: 'section' });
-  const container = createElement({ tag: 'div', className: 'container' });
+import Posts from '../components/posts/CreatePosts';
+export function createPostsPage() {
+  const root = document.getElementById('page-content');
+  root.innerHTML = '';
+  const section = createElement({ tag: 'section', className: 'posts' });
+  const container = createElement({
+    tag: 'div',
+    className: clsx('container', styles.posts__container),
+  });
   const title = createElement({ tag: 'h2', content: 'Posts' });
 
-  container.appendChild(title);
-  section.appendChild(container);
-  root.appendChild(section);
+  new Posts(container);
+  appendFabric([
+    [section, container],
+    [container, title],
+    [root, section],
+  ]);
 }
