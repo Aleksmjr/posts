74ddcc2c959323bbb2f7eff1924e5e56ef772b5b
«Aleksei <Aleksmjr97@gmail.com>
2025-09-05 15:02:15 +0300

    Fix: changed function in Class

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,28 +1,55 @@
-// создает содержимое внутри main, не имея отношения к header и footer
-// класс, который наследуется от pages (генерит новую разметку). При этом роутинг должен найти див рут и запихать туда этот эбаут. Определить роутинг можно через массив роутов, где каждый роут - это объект с полями path и component.
 import { createElement } from '../helpers/createElement';
 import { appendFabric } from '../helpers/appendFabric';
 import clsx from 'clsx';
 import styles from '../components/posts/postList/posts.module.scss';
 import { createButtonLoadingMorePosts } from '../components/posts/postLoadingButton/postLoadingButton';
 import PostList from '../components/posts/postList/PostList';
-export async function createPostsPage() {
-  const root = document.getElementById('page-content');
-  root.innerHTML = '';
-  const section = createElement({ tag: 'section', className: 'posts' });
-  const container = createElement({
-    tag: 'div',
-    className: clsx('container', styles.posts__container),
-  });
-  const title = createElement({ tag: 'h2', content: 'Posts' });
-  const newPostLists = new PostList(container);
 
-  appendFabric([
-    [section, container],
-    [container, title],
-    [root, section],
-  ]);
-  await newPostLists.init();
-  await newPostLists.mount();
-  createButtonLoadingMorePosts(container, newPostLists);
+export async function createPostsPage() {
+  new PostPage();
+}
+
+export class PostPage {
+  root = document.getElementById('page-content');
+  constructor() {
+    if (!this.root) {
+      return;
+    }
+    this.initPosts();
+  }
+
+  async renderPostsWithButtonLoadMore() {
+    this.newPostLists = new PostList(this.container);
+    await this.newPostLists.init();
+    await this.newPostLists.mount();
+    createButtonLoadingMorePosts(this.container, this.newPostLists);
+  }
+
+  createSectionLayout() {
+    this.section = createElement({ tag: 'section', className: 'posts' });
+    this.title = createElement({ tag: 'h2', content: 'Posts' });
+    this.container = createElement({
+      tag: 'div',
+      className: clsx('container', styles.posts__container),
+    });
+  }
+
+  clearRoot() {
+    this.root.innerHTML = '';
+  }
+
+  appendElements() {
+    appendFabric([
+      [this.section, this.container],
+      [this.container, this.title],
+      [this.root, this.section],
+    ]);
+  }
+
+  async initPosts() {
+    this.clearRoot();
+    this.createSectionLayout();
+    this.appendElements();
+    this.renderPostsWithButtonLoadMore();
+  }
 }
