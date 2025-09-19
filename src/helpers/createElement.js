eeee7b8d1581039b9f22f613b9ce0419b0a86b6e
«Aleksei <Aleksmjr97@gmail.com>
2025-09-19 14:26:54 +0300

    Update: update createElement

--- src/helpers/createElement.js
+++ src/helpers/createElement.js
@@ -19,3 +19,51 @@ export function createElement(props) {
 
   return el;
 }
+
+export class Element {
+  constructor(props) {
+    if (!props.tag) {
+      return;
+    }
+
+    this.el = document.createElement(props.tag);
+
+    props.content && (this.el.innerHTML = props.content);
+
+    props.className && (this.el.className = props.className);
+
+    props.href && props.tag === 'a' && (this.el.href = props.href);
+
+    if (props.attrs) {
+      Object.entries(props.attrs).forEach((item) => {
+        this.el.setAttribute(item[0], item[1]);
+      });
+    }
+  }
+
+  get textContent() {
+    return this.el.textContent;
+  }
+
+  set textContent(value) {
+    this.el.textContent = value;
+  }
+
+  get className() {
+    return this.el.classList;
+  }
+
+  set className(value) {
+    this.el.classList.add(value);
+  }
+
+  set href(value) {
+    this.el.href = value;
+  }
+
+  set attrs(value) {
+    Object.entries(value).forEach((item) => {
+      this.el.setAttribute(item[0], item[1]);
+    });
+  }
+}
